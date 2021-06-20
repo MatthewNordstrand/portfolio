3150a9fa27ef8611ccd4393915044880b65c8114
Matthew Nordstrand <matt_nordstrand@live.com>
2021-06-20 19:34:43 -0400

    Fixed issues with deployment of new assets

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.getElementById("viewBtn").addEventListener("click", showMore);
 showMore(); //Delete this line before deployment.
 
 function showMore() {
-    let frame = document.getElementById("nameContainer").classList.add("disappear-out");
+    document.getElementById("nameContainer").classList.add("disappear-out");
 
     setTimeout(switchFrames, 1000);
 }
@@ -11,7 +11,7 @@ function showMore() {
 function switchFrames() {
     document.getElementById("nameContainer").style.display = "none";
 
-    let portfolioContainer = document.getElementById("portfolioContainer");
+    var portfolioContainer = document.getElementById("portfolioContainer");
     portfolioContainer.style.display = "block";
     portfolioContainer.classList.add("appear-in");
 }
