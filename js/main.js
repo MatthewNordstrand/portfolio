b77a225b601bd72cfe718e920d00b203afc33213
Matthew Nordstrand <matt_nordstrand@live.com>
2021-06-20 18:28:19 -0400

    Transition to portfolio page seems to work now

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 function NotifyOfTest() {
-    alert("I don't have a portfolio yet :( I currently just made this site to test for now.");
+    let frame = document.getElementById("nameContainer").classList.add("disappear-out");
+
+    setTimeout(switchFrames, 1000);
+}
+
+function switchFrames() {
+    document.getElementById("nameContainer").style.display = "none";
+
+    let portfolioContainer = document.getElementById("portfolioContainer");
+    portfolioContainer.style.display = "block";
+    portfolioContainer.classList.add("appear-in");
 }
